54c67024a2f33452b443199bedb99bccfa83a3fb
ilya smagin <ilya-z1999@ya.ru>
2022-12-21 04:32:42 +0300

    day full render check

--- pages/calendar/adventDay.js
+++ pages/calendar/adventDay.js
@@ -11,37 +11,41 @@ const AdventDay = ({ day, startDay }) => {
     }
   };
   return (
-    <div
-      className="relative aspect-square w-full overflow-hidden rounded md:w-52"
-      onClick={handleClick}
-    >
-      {day?.imageB64 && (
+    <>
+      {day ? (
         <div
-          className="absolute h-full w-full bg-cover "
-          style={{
-            backgroundImage:
-              "url('data:image/jpeg;base64, " + day.imageB64 + "')",
-          }}
+          className="relative aspect-square w-full overflow-hidden rounded md:w-52"
+          onClick={handleClick}
         >
-          <p className=" absolute inset-x-4 bottom-4 rounded bg-slate-100/80 p-4 text-lg font-bold">
-            {day.text}
-          </p>
+          <div
+            className="absolute h-full w-full bg-cover "
+            style={{
+              backgroundImage:
+                "url('data:image/jpeg;base64, " + day.imageB64 + "')",
+            }}
+          >
+            <p className=" absolute inset-x-4 bottom-4 rounded bg-slate-100/80 p-4 text-lg font-bold">
+              {day.text}
+            </p>
+          </div>
+          <div
+            className={
+              "relative z-10 flex h-full w-full items-center justify-center transition-opacity duration-500 " +
+              (day.index % 2 === 0
+                ? "bg-red"
+                : day.index % 3 === 0
+                ? "bg-green"
+                : "bg-slate") +
+              (isOpened === false ? " opacity-100" : " opacity-0")
+            }
+          >
+            <h2 className="text-6xl font-semibold">{dayIs + day.index}</h2>
+          </div>
         </div>
+      ) : (
+        <div></div>
       )}
-      <div
-        className={
-          "relative z-10 flex h-full w-full items-center justify-center transition-opacity duration-500 " +
-          (day.index % 2 === 0
-            ? "bg-red"
-            : day.index % 3 === 0
-            ? "bg-green"
-            : "bg-slate") +
-          (isOpened === false ? " opacity-100" : " opacity-0")
-        }
-      >
-        <h2 className="text-6xl font-semibold">{dayIs + day.index}</h2>
-      </div>
-    </div>
+    </>
   );
 };
 export default AdventDay;
